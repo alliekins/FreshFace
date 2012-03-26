114ee4b597251c0dece1221c8d521998a2380a01
Brian Wyant <wyantb@gmail.com>
2012-03-26 14:30:40 -0400

    FreshFace, initial actual commit w/ MVC3.

---
+++ FreshFace/FreshFace/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FreshFace.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to FreshFace!  More to come!";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
