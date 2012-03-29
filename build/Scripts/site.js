eb9029dc8e686f1e34a1e99b5bef5ee0568b687d
Brian Wyant <wyantb@gmail.com>
2012-03-29 19:35:25 -0400

    Adding build version of FreshFace.

---
+++ build/Scripts/site.js
@@ -0,0 +1,16 @@
+﻿
+
+
+$(document).ready(function () {
+    console.log("Accessing service...");
+
+    $.get("../../Stock/Details/5", function (data) {
+        console.log("Accessed data: " + data);
+        console.log("Company accessed: " + data.CompanyName);
+        console.log("Company change: " + data.ChangePrice);
+        console.log("Company price: " + data.CurrentPrice);
+
+    });
+
+    console.log("Accessed service.");
+});
