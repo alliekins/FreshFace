42cdeefc9fd9f235767a977a58a383bfecbb08f7
Brian Wyant <wyantb@gmail.com>
2012-03-29 19:46:05 -0400

    Minor changes.

---
+++ FreshFace/FreshFace/Scripts/fb_calendar.js
@@ -0,0 +1,20 @@
+﻿
+$(document).ready(function () {
+    $(".calendar").fullCalendar({
+        editable: false,
+        header: {
+            left: 'title',
+            center: '',
+            right: 'next,prev today',
+        },
+    });
+
+    $(".fc-widget-content").hover(
+        function (ev) { // Hovering in
+            $(this).toggleClass("fc-hover", true);
+        }, 
+        function (ev) { // Hovering out
+            $(this).toggleClass("fc-hover", false);
+        }
+    );
+});
