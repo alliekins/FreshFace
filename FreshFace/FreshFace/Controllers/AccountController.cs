3aa411a0eede7d1c1362520e6ee157ef21d479df
Brian Wyant <wyantb@gmail.com>
2012-04-07 17:46:06 -0400

    Logging in/out is legit now.

--- FreshFace/FreshFace/Controllers/AccountController.cs
+++ FreshFace/FreshFace/Controllers/AccountController.cs
@@ -28,88 +28,27 @@ namespace FreshFace.Controllers
         // POST: /Account/LogOn
 
         [HttpPost]
-        public ActionResult LogOn(LogOnModel model, string returnUrl)
+        public JsonResult LogOn(FBModel model, string returnUrl)
         {
-            // TODO remove hack
-            // You login?  You authenticated!  Yay!  Remove before we have a DB or anything.
+            if (model != null && !model.UserID.Equals(""))
             {
-                FormsAuthentication.SetAuthCookie(model.UserName, false);
-                return RedirectToAction("Index", "Home");
-            }
-
-            if (ModelState.IsValid)
-            {
-                if (Membership.ValidateUser(model.UserName, model.Password))
-                {
-                    FormsAuthentication.SetAuthCookie(model.UserName, model.RememberMe);
-                    if (Url.IsLocalUrl(returnUrl) && returnUrl.Length > 1 && returnUrl.StartsWith("/")
-                        && !returnUrl.StartsWith("//") && !returnUrl.StartsWith("/\\"))
-                    {
-                        return Redirect(returnUrl);
-                    }
-                    else
-                    {
-                        return RedirectToAction("Index", "Home");
-                    }
-                }
-                else
-                {
-                    ModelState.AddModelError("", "The user name or password provided is incorrect.");
-                }
+                FormsAuthentication.SetAuthCookie(model.UserID, false);
+                return Json(new LogOnModel() { Success = true });
             }
 
             // If we got this far, something failed, redisplay form
-            return View(model);
+            return Json(new LogOnModel() { Success = false });
         }
 
         //
-        // GET: /Account/LogOff
+        // POST: /Account/LogOff
 
-        public ActionResult LogOff()
+        [HttpPost]
+        public JsonResult LogOff()
         {
             FormsAuthentication.SignOut();
 
-            return RedirectToAction("Index", "Home");
+            return Json(new LogOnModel() { Success = true });
         }
-
-        #region Status Codes
-        private static string ErrorCodeToString(MembershipCreateStatus createStatus)
-        {
-            // See http://go.microsoft.com/fwlink/?LinkID=177550 for
-            // a full list of status codes.
-            switch (createStatus)
-            {
-                case MembershipCreateStatus.DuplicateUserName:
-                    return "User name already exists. Please enter a different user name.";
-
-                case MembershipCreateStatus.DuplicateEmail:
-                    return "A user name for that e-mail address already exists. Please enter a different e-mail address.";
-
-                case MembershipCreateStatus.InvalidPassword:
-                    return "The password provided is invalid. Please enter a valid password value.";
-
-                case MembershipCreateStatus.InvalidEmail:
-                    return "The e-mail address provided is invalid. Please check the value and try again.";
-
-                case MembershipCreateStatus.InvalidAnswer:
-                    return "The password retrieval answer provided is invalid. Please check the value and try again.";
-
-                case MembershipCreateStatus.InvalidQuestion:
-                    return "The password retrieval question provided is invalid. Please check the value and try again.";
-
-                case MembershipCreateStatus.InvalidUserName:
-                    return "The user name provided is invalid. Please check the value and try again.";
-
-                case MembershipCreateStatus.ProviderError:
-                    return "The authentication provider returned an error. Please verify your entry and try again. If the problem persists, please contact your system administrator.";
-
-                case MembershipCreateStatus.UserRejected:
-                    return "The user creation request has been canceled. Please verify your entry and try again. If the problem persists, please contact your system administrator.";
-
-                default:
-                    return "An unknown error occurred. Please verify your entry and try again. If the problem persists, please contact your system administrator.";
-            }
-        }
-        #endregion
     }
 }
--- FreshFace/FreshFace/Models/FBModel.cs
+++ FreshFace/FreshFace/Models/FBModel.cs
@@ -2,11 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace FreshFace.Models
 {
     public class FBModel
     {
+        [Required]
+        [Display(Name = "FB User Id")]
+        public string UserID { get; set; }
+
         public static string GetAppID(HttpContextBase server)
         {
             var AppID = "377609208946698";
@@ -16,6 +21,18 @@ namespace FreshFace.Models
                 // But if you provide your own ID in this file 
                 //  (root/fb_app_id.txt) , use it
                 var filePath = server.Server.MapPath("../fb_app_id.txt");
+                AppID = System.IO.File.ReadAllText(filePath);
+            }
+            catch (Exception)
+            {
+                // Don't care otherwise; use the default.
+            }
+
+            try
+            {
+                // But if you provide your own ID in this file 
+                //  (root/fb_app_id.txt) , use it
+                var filePath = server.Server.MapPath("fb_app_id.txt");
                 AppID = System.IO.File.ReadAllText(filePath);
             }
             catch (Exception)
