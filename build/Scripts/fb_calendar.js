fed70b31b72432e548163db813c211ce72da5b9c
Brian Wyant <wyantb@gmail.com>
2012-04-07 17:52:08 -0400

    Ready a new build.

--- build/Scripts/fb_calendar.js
+++ build/Scripts/fb_calendar.js
@@ -5,16 +5,7 @@ $(document).ready(function () {
         header: {
             left: 'title',
             center: '',
-            right: 'next,prev today',
-        },
-    });
-
-    $(".fc-widget-content").hover(
-        function (ev) { // Hovering in
-            $(this).toggleClass("fc-hover", true);
-        }, 
-        function (ev) { // Hovering out
-            $(this).toggleClass("fc-hover", false);
+            right: 'next,prev today'
         }
-    );
+    });
 });
