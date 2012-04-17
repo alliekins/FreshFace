a2a7a1fc3443bd89bfba432a191c7ff0563efc02
Brian Wyant <wyantb@gmail.com>
2012-04-17 14:20:47 -0400

    Let's try using local storage instead of cookies.

--- FreshFace/FreshFace/Scripts/FreshFace.js
+++ FreshFace/FreshFace/Scripts/FreshFace.js
@@ -14,11 +14,11 @@
 var FreshFace = {
 
     /**
-     * Log out of session on server.
-     *
-     * @method logOff
-     *
-     */
+    * Log out of session on server.
+    *
+    * @method logOff
+    *
+    */
     logOff: function () {
         $.post('../Account/LogOff', function (resp) {
             // Logging off is always successful, no need to check response
@@ -27,33 +27,62 @@ var FreshFace = {
     },
 
     /**
-     * Log into a session on server, given FB data.
-     *
-     * @method logOn
-     * @param (Object) data the data to send to server
-     *
-     */
+    * Log into a session on server, given FB data.
+    *
+    * @method logOn
+    * @param (Object) data the data to send to server
+    *
+    */
     logOn: function (data) {
         $.post("../Account/LogOn", data, function (resp) {
             if (resp.Success) {
                 window.location.href = '../Home/Index';
             }
         });
+    },
+
+    /**
+    * Removes the given stock.
+    *
+    * @method removeStock
+    *
+    */
+    removeStock: function (name) {
+        var stockStr = localStorage.getItem("MyStocks");
+        var stocks = JSON.parse(stockStr);
+        var index = stocks.indexOf(name);
+        if (index != -1) {
+            stocks.splice(index, 1);
+        }
+        localStorage.setItem("MyStocks", JSON.stringify(stocks));
+    },
+
+    /**
+    * Removes the given stock.
+    *
+    * @method removeStock
+    *
+    */
+    addStock: function (name) {
+        var stockStr = localStorage.getItem("MyStocks");
+        var stocks = JSON.parse(stockStr);
+        stocks.push(name);
+        localStorage.setItem("MyStocks", JSON.stringify(stocks));
     }
+
 };
 
 $(document).ready(function () {
 
-    var s = getCookie("curStocks");
-    var stocks;
-
-    if (s == null) {
-        stocks = new Array("GOOG", "MSFT", "AAPL", "AMZN");
-        setCookie("curStocks", stocks, 7);
-    } else if (s.length == "") {
-        $("#stocks").append("<tr><td>Add</td><td>Stocks</td><td>Below</td><tr>");
+    var stocks = [];
+    if (localStorage.getItem("MyStocks") === null ||
+        typeof localStorage.getItem("MyStocks") === "undefined") {
+        stocks = ["GOOG", "MSFT", "AAPL", "AMZN"];
+        stockStr = JSON.stringify(stocks);
+        localStorage.setItem("MyStocks", stockStr);
     } else {
-        stocks = s.split(',');
+        var stockStr = localStorage.getItem("MyStocks");
+        stocks = JSON.parse(stockStr);
     }
 
     for (var i = 0; i < stocks.length; i++) {
@@ -62,8 +91,8 @@ $(document).ready(function () {
         $.get("../Stock/Details/" + stocks[i], function (data) {
             $("#stocks").append("<tr><td><a href=\"http://finance.yahoo.com/q?s=" + data.CompanyName + "\">"
             + data.CompanyName + "</a></td><td>" + data.CurrentPrice.toFixed(2) + "</td><td> "
-             + data.ChangePrice.toFixed(2) + "</td><td><a id=\"" + data.CompanyName 
-             + "\" title=\"Remove "+ data.CompanyName +"\" class=\"remove\" href=\"\">X</a></td></tr>");
+             + data.ChangePrice.toFixed(2) + "</td><td><a id=\"" + data.CompanyName
+             + "\" title=\"Remove " + data.CompanyName + "\" class=\"remove\" href=\"\">X</a></td></tr>");
 
         });
     }
@@ -81,27 +110,3 @@ $(document).ready(function () {
         }
     });
 });
-
-//Function that creates a cookie
-//http://www.w3schools.com/js/js_cookies.asp
-function setCookie(c_name, value, exdays) {
-    var exdate = new Date();
-    exdate.setDate(exdate.getDate() + exdays);
-    var c_value = escape(value) + ((exdays == null) ? "" : "; expires=" + exdate.toUTCString());
-    document.cookie = c_name + "=" + c_value;
-}
-
-//Function that getsa cookie
-//http://www.w3schools.com/js/js_cookies.asp
-function getCookie(c_name) {
-    var i, x, y, ARRcookies = document.cookie.split(";");
-    for (i = 0; i < ARRcookies.length; i++) {
-        x = ARRcookies[i].substr(0, ARRcookies[i].indexOf("="));
-        y = ARRcookies[i].substr(ARRcookies[i].indexOf("=") + 1);
-        x = x.replace(/^\s+|\s+$/g, "");
-        if (x == c_name) {
-            return unescape(y);
-        }
-    }
-}
-
--- build/Scripts/FreshFace.js
+++ build/Scripts/FreshFace.js
@@ -10,14 +10,15 @@
 * @global
 *
 */
+
 var FreshFace = {
 
     /**
-     * Log out of session on server.
-     *
-     * @method logOff
-     *
-     */
+    * Log out of session on server.
+    *
+    * @method logOff
+    *
+    */
     logOff: function () {
         $.post('../Account/LogOff', function (resp) {
             // Logging off is always successful, no need to check response
@@ -26,34 +27,75 @@ var FreshFace = {
     },
 
     /**
-     * Log into a session on server, given FB data.
-     *
-     * @method logOn
-     * @param (Object) data the data to send to server
-     *
-     */
+    * Log into a session on server, given FB data.
+    *
+    * @method logOn
+    * @param (Object) data the data to send to server
+    *
+    */
     logOn: function (data) {
         $.post("../Account/LogOn", data, function (resp) {
             if (resp.Success) {
                 window.location.href = '../Home/Index';
             }
         });
+    },
+
+    /**
+    * Removes the given stock.
+    *
+    * @method removeStock
+    *
+    */
+    removeStock: function (name) {
+        var stockStr = localStorage.getItem("MyStocks");
+        var stocks = JSON.parse(stockStr);
+        var index = stocks.indexOf(name);
+        if (index != -1) {
+            stocks.splice(index, 1);
+        }
+        localStorage.setItem("MyStocks", JSON.stringify(stocks));
+    },
+
+    /**
+    * Removes the given stock.
+    *
+    * @method removeStock
+    *
+    */
+    addStock: function (name) {
+        var stockStr = localStorage.getItem("MyStocks");
+        var stocks = JSON.parse(stockStr);
+        stocks.push(name);
+        localStorage.setItem("MyStocks", JSON.stringify(stocks));
     }
+
 };
 
-
-// EXAMPLE CODE ONLY
-// Getting a stock from the server
 $(document).ready(function () {
-    Debug.log("Accessing service...");
 
-    $.get("../Stock/Details/5", function (data) {
-        Debug.log("Accessed data: " + data);
-        Debug.log("Company accessed: " + data.CompanyName);
-        Debug.log("Company change: " + data.ChangePrice);
-        Debug.log("Company price: " + data.CurrentPrice);
+    var stocks = [];
+    if (localStorage.getItem("MyStocks") === null ||
+        typeof localStorage.getItem("MyStocks") === "undefined") {
+        stocks = ["GOOG", "MSFT", "AAPL", "AMZN"];
+        stockStr = JSON.stringify(stocks);
+        localStorage.setItem("MyStocks", stockStr);
+    } else {
+        var stockStr = localStorage.getItem("MyStocks");
+        stocks = JSON.parse(stockStr);
+    }
 
-    });
+    for (var i = 0; i < stocks.length; i++) {
+
+        var url = "../Stock/Details/" + stocks[i];
+        $.get("../Stock/Details/" + stocks[i], function (data) {
+            $("#stocks").append("<tr><td><a href=\"http://finance.yahoo.com/q?s=" + data.CompanyName + "\">"
+            + data.CompanyName + "</a></td><td>" + data.CurrentPrice.toFixed(2) + "</td><td> "
+             + data.ChangePrice.toFixed(2) + "</td><td><a id=\"" + data.CompanyName
+             + "\" title=\"Remove " + data.CompanyName + "\" class=\"remove\" href=\"\">X</a></td></tr>");
+
+        });
+    }
 });
 
 // Turns calendars into FullCalendars
